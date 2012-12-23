377af82d25dc59bab50349d48a92483783d1d2b5
AquarHEAD L <aquarhead@gmail.com>
2012-12-24 04:09:18 +0800

    test down

---
+++ README.md
@@ -0,0 +1,16 @@
+Miaopull
+=====
+
+Intro
+-----
+
+｢喵噗哦｣是由Flask驅動的自動pull工具.
+
+Miaopull is an auto-pull util upon Flask.
+
+Setup
+-----
+
+(待補充
+
+(to do
--- miao.py
+++ miao.py
@@ -1,13 +1,52 @@
 # coding=utf8
 
-from flask import Flask
+from flask import Flask, request
+from subprocess import call
+import ujson
 
 app = Flask(__name__)
-app.debug = True
+
+f = open("miao.json", 'r')
+conf = ujson.loads(f.read())
+f.close()
+
+git_pull = '" && git reset --hard {} && git pull'
+hg_pull = '" && hg pull' # need to reset changes
+
+def do_pull(type, path, branch):
+    if type == 'git':
+        call(['cd "' + path + git_pull.format(branch)], shell=True)
+    else:
+        call(['cd "' + path + hg_pull], shell=True)
 
 @app.route("/welcome")
-def hello():
-    return u"｢喵噗兒｣"
+def welcome():
+    return u"｢喵噗哦｣"
+
+@app.route('/miao', methods=['POST'])
+def miaopull():
+    payload = ujson.load(request.form['payload'])
+    if 'canon_url' in payload:
+        ptype = 'bitbucket'
+        pbranch = payload['commits'][0]['branch']
+        purl = payload['canon_url']+payload['repository']['absolute_url']
+    else:
+        ptype = 'github'
+        pbranch = payload['ref'].split('/')[-1]
+        purl = payload['repository']['url']
+
+    for r in conf['repos']:
+        rurl = r['url']
+        rpath = r['path']
+        rwatch = r['watch']
+        if rurl == purl:
+            for b in rwatch:
+                if b == pbranch:
+                    if ptype == 'bitbucket':
+                        do_pull(payload['repository']['scm'], rpath, b)
+                    else:
+                        do_pull('git', rpath, b)
+    return u"｢喵｣"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=conf['port'])
