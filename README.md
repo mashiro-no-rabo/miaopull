fa46a1332cec7899599ed9815f9b213aa3212051
AquarHEAD L <aquarhead@gmail.com>
2012-12-26 03:13:02 +0800

    major update, support email notify

--- README.md
+++ README.md
@@ -5,32 +5,111 @@ Intro
 -----
 Miaopull is an auto-pull util upon Flask.
 
-She can watch multiple branches of multiple repos, if any of these changed she will pull the newest code from Github or Bitbucket automatically.
+She can watch multiple repos on Github or Bitbucket, automatically pull the newest code on the watching branch, build or deploy with your custom commands, and send emails to your team once everything works fine.
 
 ｢簡介｣
 -----
 ｢喵噗哦｣是由Flask驅動的自動代碼更新工具.
 
-她可以監視多個代碼庫的多個分支, 若其中任一有更新即會自動下載, 支持Github和Bitbucket.
+她可以監視來自Github或Bitbucket的多個代碼庫, 自動下載指定分支上的最新代碼並執行自定義命令來構建或部署你的代碼, 最後發郵件通知你的團隊.
 
 Setup
 -----
 
 1. clone the repo to your server
 2. change `miao.json` accrodingly
-3. make a virtualenv and activate it
+3. make a `virtualenv` and activate it
 4. `pip install -r requirements.txt`
 5. `python miao.py &`
 6. setup hooks and add your url
    - Github: `Settings` > `Service Hooks` > `WebHook URLs`
    - Bitbucket: `admin` > `Services` > `POST`
 
-｢配置｣
+｢安裝｣
 -----
 
 1. 下載代碼至服務器
 2. 根據需要修改配置文件`miao.json`
-3. 創建`virtualenv`並激活
+3. 創建虛擬環境並激活(推薦使用`virtualenv`, 否則`pip`指令可能需要`root`權限)
 4. 同上
 5. 同上
 6. 配置鉤子(具體方法同上)
+
+About `miao.json`
+-----
+
+```
+{
+    "port": 8088, # on which port miaopull runs
+    "email_notify": false, # true to enable sending email
+    "email_settings": {
+    # safe to ignore these if not sending email
+        "host": "smtp.gmail.com",
+        "port": 465,
+        "mode": "ssl",
+        "login": "your_account@example.com",
+        "password": "your_gmail_password",
+        "sender_name": "｢喵｣",
+        "sender_email": "someone@example.com",
+        "default_recipients": [
+        # if no specified recipients use these
+            "someone@example.com",
+            "someone_else@example.com"
+        ]
+    },
+    "repos": [
+        {
+            "path": "/Users/aquarhead/Projects/miaopull",
+            "branch": "master",
+            "commands": [
+            # custom commands to execute after pull
+            # working dir is "path"
+                "mv settings_server.py settings.py",
+                "mv consts_server.py consts.py"
+            ],
+            "specify_recipients": [
+                "someone@example.com"
+            ]
+        }
+    ]
+}
+```
+
+｢配置 `miao.json`｣
+-----
+
+```
+{
+    "port": 8088, # 喵運行的端口
+    "email_notify": false, # 設置爲true來啓用郵件通知
+    "email_settings": {
+    # 若未啓用郵件通知可以忽略
+        "host": "smtp.gmail.com",
+        "port": 465,
+        "mode": "ssl",
+        "login": "your_account@example.com",
+        "password": "your_gmail_password",
+        "sender_name": "｢喵｣",
+        "sender_email": "someone@example.com",
+        "default_recipients": [
+        # 若repo沒有設置收件人則用下列默認
+            "someone@example.com",
+            "someone_else@example.com"
+        ]
+    },
+    "repos": [
+        {
+            "path": "/Users/aquarhead/Projects/miaopull",
+            "branch": "master",
+            "commands": [
+            # pull後需要執行的命令, 工作目錄同上面設置的"path"
+                "mv settings_server.py settings.py",
+                "mv consts_server.py consts.py"
+            ],
+            "specify_recipients": [
+                "someone@example.com"
+            ]
+        }
+    ]
+}
+```
--- miao.py
+++ miao.py
@@ -3,6 +3,7 @@
 from flask import Flask, request
 from subprocess import call
 import ujson
+import pyzmail
 
 app = Flask(__name__)
 
@@ -10,39 +11,73 @@ f = open("miao.json", 'r')
 conf = ujson.loads(f.read())
 f.close()
 
+if conf['email_notify']:
+    for r in conf['repos']:
+        if "specify_recipients" in r:
+            r['recipients'] = r['specify_recipients']
+        else:
+            r['recipients'] = conf['email_settings']['default_recipients']
+
 git_pull = '" && git reset --hard {} && git pull'
 hg_pull = '" && hg update -C && hg pull'
 
-def do_pull(type, path, branch, command):
-    if type == 'git':
-        call(['cd "' + path + git_pull.format(branch) + ' && ' + command], shell=True)
+def do_pull(repo, c):
+    if repo['type'] == 'git':
+        call(['cd "' + repo['path'] + git_pull.format(repo['branch']) + ' && ' + repo['command']], shell=True)
     else:
-        call(['cd "' + path + hg_pull + ' && ' + command], shell=True)
+        call(['cd "' + repo['path'] + hg_pull + ' && ' + repo['command']], shell=True)
+    if conf['email_notify']:
+        sender = (conf['email_settings']['sender_name'], conf['email_settings']['sender_email'])
+        recipients = repo['recipients']
+        subject=u'｢喵｣ triggered on '+c['name']+':'+repo['branch']
+        text_content='\n'.join([u'Miaopull was triggered by the commit', u"｢喵噗哦｣被以下提交觸發:", 'Message: '+c['msg'], 'ID: '+c['id']])
+        prefered_encoding='utf-8'
+        text_encoding='utf-8'
+
+        payload, mail_from, rcpt_to, msg_id = pyzmail.compose_mail(sender, recipients, subject, prefered_encoding, (text_content, text_encoding), html=None)
+
+        ret=pyzmail.send_mail(payload, mail_from, rcpt_to, conf['email_settings']['host'], smtp_port=conf['email_settings']['port'], smtp_mode=conf['email_settings']['mode'], smtp_login=conf['email_settings']['login'], smtp_password=conf['email_settings']['password'])
+
+        if isinstance(ret, dict):
+            if ret:
+                print '[email notify] failed recipients:', ', '.join(ret.keys())
+            else:
+                print '[email notify] success'
+        else:
+            print '[email notify] error:', ret
 
 @app.route("/welcome")
 def welcome():
-    return u"｢喵噗哦｣"
+    return u"Miaopull｢喵噗哦｣"
 
 @app.route('/miao', methods=['POST'])
 def miaopull():
     payload = ujson.loads(request.form['payload'])
+    c = {}
     if 'canon_url' in payload:
         ptype = 'bitbucket'
         pbranch = payload['commits'][0]['branch']
+        c['name'] = payload['repository']['name']
+        c['msg'] = payload['commits'][0]['message']
+        c['id'] = payload['commits'][0]['raw_node']
     else:
         ptype = 'github'
         pbranch = payload['ref'].split('/')[-1]
+        c['name'] = payload['repository']['name']
+        c['msg'] = payload['commits'][0]['message']
+        c['id'] = payload['commits'][0]['id']
 
     for r in conf['repos']:
         rpath = r['path']
         rbranch = r['branch']
         if rbranch == pbranch:
             rcommands = r['commands']
-            rcommand = ' && '.join(rcommands)
+            r['command'] = ' && '.join(rcommands)
             if ptype == 'bitbucket':
-                do_pull(payload['repository']['scm'], rpath, rbranch, rcommand)
+                r['type'] = payload['repository']['scm']
             else:
-                do_pull('git', rpath, rbranch, rcommand)
+                r['type'] = 'git'
+            do_pull(r, c)
     return u"｢喵｣"
 
 if __name__ == "__main__":
